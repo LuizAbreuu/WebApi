0642d96dcb9fd2c4a245cd4643abb2bfa7b2d262
LuizAbreuu <luiz_mullercosta@yahoo.com.br>
2024-12-16 18:16:55 -0300

    primeira versão

---
+++ WebApi/Models/ResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using WebApi.Migrations;
+
+namespace WebApi.Models
+{
+    public class ResponseModel<T>
+    {
+        public T? Dados { get; set; }
+        public string Mensagem { get; set; } = string.Empty;
+        public bool Status { get; set; } = true;
+    }
+}
